f334d8e9437a83b7a07aaa58981014177e03cd23
Pepe Doval <pepellou@gmail.com>
2012-05-07 14:16:38 +0200

    Add tests

---
+++ spec/theGame.js
@@ -0,0 +1,14 @@
+describe("The MuertosYHeridos game", function () {
+
+	beforeEach(function () {
+	});
+
+
+	afterEach(function () {
+	});
+
+	it("is a game", function () {
+		expect(true).toBeFalsy();
+	});
+
+});
